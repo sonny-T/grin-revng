8a0e955a0ac56c5367fc88b08f2bd83eb44e71cb
sonnylier <sonny_lier@163.com>
2021-05-20 12:46:50 +0800

    continue to break in handleGlobalDataGadget

--- tools/revng-lift/JumpTargetManager.cpp
+++ tools/revng-lift/JumpTargetManager.cpp
@@ -2970,7 +2970,7 @@ void JumpTargetManager::handleGlobalDataGadget(llvm::BasicBlock *thisBlock,std::
             if(*ptc.isIndirect or *ptc.isIndirectJmp)
               assign_gadge[i].second.indirect = true;  
             harvestCodePointerInDataSegment(i);
-            continue;
+            break;
           }
           auto result = getGlobalDatafromRegs(&*it,i);
           if(result){
@@ -2981,7 +2981,7 @@ void JumpTargetManager::handleGlobalDataGadget(llvm::BasicBlock *thisBlock,std::
               }
               assign_gadge[i].second.end = false;
               AllGadget[thisBlock] = 1;
-              continue;
+              break;
           }
 
           //Restore original assign_gadge info
